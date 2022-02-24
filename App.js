cb6ceba58f43a95a8b4c09c5d3ff427a2c5214df
sone <sthaya@gmail.com>
2022-02-24 14:46:43 -0800

    setup navigation, background image and login

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
+
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 import {
   useFonts as useOswald,
@@ -13,8 +20,36 @@ import { Navigation } from "./src/infrastructure/navigation";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlQvq5Jc2oIRkc_Rl_R8w7SiGIFkI1KC8",
+  authDomain: "mealstogo-611e8.firebaseapp.com",
+  projectId: "mealstogo-611e8",
+  storageBucket: "mealstogo-611e8.appspot.com",
+  messagingSenderId: "431118819944",
+  appId: "1:431118819944:web:0b97dbdf890fc6ac6dd1dd",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      signInWithEmailAndPassword(auth, "mo@binni.io", "test123")
+        .then((user) => {
+          console.log("********", user);
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    }, 2000);
+  }, []);
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -27,16 +62,20 @@ export default function App() {
     return null;
   }
 
+  if (!isAuthenticated) return null;
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
---
+++ src/services/authentication/authentication.service.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlQvq5Jc2oIRkc_Rl_R8w7SiGIFkI1KC8",
+  authDomain: "mealstogo-611e8.firebaseapp.com",
+  projectId: "mealstogo-611e8",
+  storageBucket: "mealstogo-611e8.appspot.com",
+  messagingSenderId: "431118819944",
+  appId: "1:431118819944:web:0b97dbdf890fc6ac6dd1dd",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export const loginRequest = (email, password) =>
+  signInWithEmailAndPassword(auth, email, password);
